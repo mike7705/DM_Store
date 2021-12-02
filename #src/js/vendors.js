0280214abbcedda45110c24fd7eee8831c90ad58
mike7705 <mikekom@mail.ru>
2021-12-02 22:27:33 +0300

    initial

--- #src/js/vendors.js
+++ #src/js/vendors.js
@@ -1,2 +1,3 @@
-@@include('libs/smoothScroll.js', {})
-@@include('libs/navigation.min.js', {})
+@@include('libs/smoothScroll.js', {});
+@@include('libs/navigation.min.js', {});
+@@include('libs/swiper.min.js', {});
